1ecaa8b93a525334e14091dc5331b4fe92954d6a
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-12 12:09:24 +0900

    삼각형 별 찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            for (int i = 7; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int k = 1; k < 16 - 2 * i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+                
+            }
         }
     }
 }
